2a055f0bfd724a81defa2c418a3a5c6b827809c9
Somnath Shekhar <snshekhar@paypal.com>
2017-05-26 00:16:26 -0700

    Validating and Parsing Email Addresses

---
+++ email_validator.py
@@ -0,0 +1,18 @@
+# Sample Input
+# 3
+# DEXTER <dexter@hotmail.com>
+# SOM <som@gmail.com>
+# VIRUS <virus!@variable.:p>
+
+# Sample Output
+# DEXTER <dexter@hotmail.com>
+# SOM <som@gmail.com>
+from email.utils import parseaddr
+import re
+
+
+pattern = "^[a-zA-Z][\w\-\.]*@[A-Za-z]+\.[a-zA-Z]{1,3}$"
+
+# Enter number followed by name and email (name <user@email.com>)
+data = [ raw_input() for count in range(int(raw_input())) ]
+print '\n'.join([ email for email in data if re.match(pattern, parseaddr(email)[1]) ])
