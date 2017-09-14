3ce1ab20a22700e7bb97c8b39c3a158a3938ec75
Somnath Shekhar <snshekhar@paypal.com>
2017-09-14 14:28:14 -0700

    interview question

---
+++ sum_array.py
@@ -0,0 +1,15 @@
+import itertools
+#Find a pair of elements from an array whose sum equals a given number
+
+def sum_array(A,x):
+    for num in itertools.combinations(set(A), 2):
+        if num[0] +num[1] == x:
+            return 1
+    return 0
+
+
+A = [2, 4, 6, 8, 10]
+x = 8 # True condition
+#x = 3 # False condition
+res = sum_array(A, x)
+print "exists" if res else "Doesnt exist"
