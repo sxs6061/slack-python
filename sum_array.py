3bfa0119c0bd28c01ca709e6f9153d0cb008bb79
Somnath Shekhar <snshekhar@paypal.com>
2017-09-15 11:30:24 -0700

    Refactoring

--- sum_array.py
+++ sum_array.py
@@ -3,7 +3,7 @@ import itertools
 
 def sum_array(A,x):
     for num in itertools.combinations(set(A), 2):
-        if num[0] +num[1] == x:
+        if num[0] + num[1] == x:
             return 1
     return 0
 
