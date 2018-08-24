8a395833a371a6e2016166db99ade6bb50bda16f
Somnath Shekhar <snshekhar@paypal.com>
2018-08-24 11:20:01 -0700

    adding examples

--- non-unique.py
+++ non-unique.py
@@ -1,4 +1,6 @@
-#returns a list of non-unique elements
+# returns a list of non-unique elements
+# i/p: 1 2 3 4 3
+# o/p: [3, 3]
 
 data = [int(i) for i in raw_input().split()]
 print ([ num for num in data if(data.count(num) > 1) ])
