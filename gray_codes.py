d140aa102723f1c28c22942411ef3fb2c9ec77c6
Somnath Shekhar <snshekhar@paypal.com>
2019-02-04 17:21:18 -0800

    using list comprehension

--- gray_codes.py
+++ gray_codes.py
@@ -1,9 +1,18 @@
 # Given a number of buts n, generate a possible gray codefor it.
 # Eg. for n = 2, once gray code would be  [00, 01, 10, 11]
-def main():
-    n=int(raw_input())
-    for i in range(0, 1<<n): # bitwise left same as multiplying 1 by 2**n. 
-        gray=i^(i>>1) # bitwise right same as //'ing i by 2**1
-        print "{0:0{1}b}".format(gray,n), # formatting in binary and padding 0s 
 
-main()
+# Using list comprehension:
+def gray_code(n):
+    return  ([ "{0:0{1}b}".format(i,n) for i in xrange(2**n) ])
+
+print " ".join(gray_code(2)),
+
+
+# Using Bitwise operarions:
+# def main():
+#     n=int(raw_input())
+#     for i in range(0, 1<<n):
+#         gray=i^(i>>1)
+#         print "{0:0{1}b}".format(gray,n),
+
+# main()
