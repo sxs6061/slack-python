0b0d9bcca596dfa5a8430cbaa1e714844ecc0c4c
Somnath Shekhar <snshekhar@paypal.com>
2019-02-27 14:21:13 -0800

    DCP

---
+++ substring_index.py
@@ -0,0 +1,14 @@
+# Given a string s and a list of words words, where each word is the same length, 
+# find all starting indices of substrings in s that is a concatenation of every word in words exactly once.
+
+# For example, given s = "dogcatcatcodecatdog" and words = ["cat", "dog"], 
+# return [0, 13], since "dogcat" starts at index 0 and "catdog" starts at index 13.
+
+# Given s = "barfoobazbitbyte" and words = ["dog", "cat"], 
+# return [] since there are no substrings composed of "dog" and "cat" in s.
+
+# The order of the indices does not matter.
+
+s = "barfoobazbitbyte"
+words = ["cat", "dog"]
+print [ s.find(i) for i in [''.join(words[::-1]), ''.join(words)] if i in s ]
