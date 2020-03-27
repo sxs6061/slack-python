a141cdb693fd344c7477830b91ad549e8de9b279
Somnath Shekhar <snshekhar@paypal.com>
2020-03-27 01:24:28 -0700

    gotcha

--- isomorphic.py
+++ isomorphic.py
@@ -2,11 +2,21 @@
 # For example, given s1 = abc and s2 = bcd, return true since we can map a to b, b to c, and c to d.
 # Given s1 = foo and s2 = bar, return false since the o cannot map to two characters.
 #
-def is_isomorphic(s1, s2):
-     if len(s1) != len(s2):
-          return False
-     return bool( len(dict(zip(s1, s2))) == len(dict(zip(s2, s1))) )
+def is_isomorphic(s, t):
+     if len(s) != len(t): return False
+     dic = {}
+     for i in range(len(s)):
+          sChar = s[i]
+          tChar = t[i]
+          if dic.has_key(sChar):
+               if dic.get(sChar) != tChar: return False
+          else:
+               if tChar in dic.values(): return False
+               dic.setdefault(sChar, tChar)
+     return True
 
-s1 = "abc" # "foo"
-s2 = "bcd" # "bar"
-print is_isomorphic(s1, s2)
+
+s = "aba" #abc
+t = "baa" #bcd
+
+print is_isomorphic(s, t)
