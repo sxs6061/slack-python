7f0036ad259b4c6a3086ad4a7e3d77a06fedb9b0
snshekhar <snshekhar@paypal.com>
2024-02-26 11:20:42 -0600

    code refactor

--- email_validator.py
+++ email_validator.py
@@ -10,9 +10,8 @@
 from email.utils import parseaddr
 import re
 
-
 pattern = r"^[a-zA-Z][\w\-\.]*@[A-Za-z]+\.[a-zA-Z]{1,3}$"
 
 # Enter number followed by name and email (name <user@email.com>)
 data = [ raw_input() for count in range(int(raw_input())) ]
-print '\n'.join([ email for email in data if re.match(pattern, parseaddr(email)[1]) ])
+print('\n'.join([ email for email in data if re.match(pattern, parseaddr(email)[1]) ]))
--- list_gcd.py
+++ list_gcd.py
@@ -1,10 +1,12 @@
 # Given n numbers, find the greatest common denominator between them.
 # For example, given the numbers [42, 56, 14], return 14.
 #
+from functools import reduce
+
 def gcd(a, b): # library(fractions) function for GCD in python
     while b:
         a, b = b, a%b
     return a
 
 L = [42, 56, 14, 7]
-print reduce((lambda x, y: gcd(x,y)), L) # apply gcd to sequential pairs of values in a list
+print(reduce((lambda x, y: gcd(x,y)), L)) # apply gcd to sequential pairs of values in a list
